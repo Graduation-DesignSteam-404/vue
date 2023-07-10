3b03f06bdcfc7cbd17b0568c2f957d6ac7b8437d
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-10 18:59:37 +0800

    update
    导入element

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 const app=createApp(App)
 
+import element from 'element-plus'
+import 'element-plus/dist/index.css'
+
+app.use(element)
 app.mount('#app')
