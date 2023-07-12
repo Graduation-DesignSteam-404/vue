7582107022d84cac5692bec75a8769280fe2f8ec
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-12 17:11:46 +0800

    append
    引入element icon库

--- src/main.js
+++ src/main.js
@@ -5,5 +5,12 @@ const app=createApp(App)
 import element from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import router from '@/router/index'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
 app.use(element)
+app.use(router)
 app.mount('#app')
