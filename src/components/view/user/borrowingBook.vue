821106b2be6900ef4f8f8b54f5b723b866c871ca
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-14 19:31:53 +0800

    Create borrowingBook.vue
    前台
    借阅图书

---
+++ src/components/view/user/borrowingBook.vue
@@ -0,0 +1,57 @@
+<!--借阅图书-->
+<template>
+    <el-input
+            v-model="book"
+            autosize
+            type="textarea"
+            placeholder="书名"
+    />
+    <el-input
+            v-model="user"
+            autosize
+            type="textarea"
+            placeholder="借阅者"
+    />
+  <el-button @click="postBorrowingBook" type="primary">借阅</el-button>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+import axios from "axios";
+import {ElMessage} from "element-plus";
+
+const borrowingBookUrl=ref('http://localhost:8080/borrowing');
+const book=ref('')
+const user=ref('')
+
+const postBorrowingBook=():void=>{
+    borrowingBookUrl.value+='?book='+book.value;
+    borrowingBookUrl.value+='&user='+user.value;
+    console.log(borrowingBookUrl.value)
+    axios.post(borrowingBookUrl.value)
+        .then((response)=>{
+            clear();
+            console.log(response)
+            if(response.data>=1){
+                ElMessage.success('借阅成功')
+            }else if(response.data===-1){
+                ElMessage.warning('图书不存在')
+            }else if(response.data===-2){
+                ElMessage.warning('用户不存在')
+            }
+        })
+        .catch((error)=>{
+            console.log(error)
+            clear();
+        })
+}
+const clear=():void=>{
+    borrowingBookUrl.value='http://localhost:8080/borrowing';
+    book.value='';
+    user.value='';
+}
+</script>
+
+<style>
+
+</style>
