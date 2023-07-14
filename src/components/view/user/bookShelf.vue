681fb1e21c5fddc057438029662275cc71789cea
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-14 19:31:35 +0800

    Create bookShelf.vue
    前台
    书架

---
+++ src/components/view/user/bookShelf.vue
@@ -0,0 +1,36 @@
+<!--书架-->
+<template>
+    <p>书架</p>
+    <div id="main">
+        <el-table :data="bookList">
+            <el-table-column prop="id" label="id"/>
+            <el-table-column prop="name" label="书名"/>
+            <el-table-column prop="author" label="作者"/>
+        </el-table>
+    </div>
+</template>
+<script setup lang="ts">
+import {ref, onMounted} from "vue";
+import axios from "axios";
+
+const allBookUrl = ref('http://localhost:8080/allBook')
+const bookList = ref('')
+
+const getBookShelf = (): void => {
+    axios.get(allBookUrl.value)
+        .then((response) => {
+            console.log(response)
+            bookList.value = response.data;
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+onMounted(() => {
+    getBookShelf();
+})
+</script>
+
+<style>
+
+</style>
